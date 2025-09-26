d71f342ee48c41b415725bcf7eac88151a1024ef
muhammadaliyev <muhammadaliyev>
2025-09-26 20:53:48 +0500

    nf

--- js/app.js
+++ js/app.js
@@ -50,7 +50,7 @@ const updateUI = function (dataArr) {
 const getData = async () => {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
-    if (!response.ok) throw new Error("Ma'lumot topilmadi");
+    if (!response.ok) throw new Error("Malumot topilmadi");
     const data = await response.json();
     users = data;
     updateUI(users);
